2a1bdf0e03a10210d451993d1b7a53b9ed0a6950
ShivamWho <shivam2094.be20@chitkara.edu.in>
2021-06-30 10:43:35 +0530

    add header, note, footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+    let currentTime = new Date();
+    let year = currentTime.getFullYear();
+
+    return(
+        <footer>
+            <p>Copyright ⓒ {year}</p>
+        </footer>
+    );
+}
+
+export default Footer;

12ca888b880588f8c0caaec56ef468fc8cf6bc60
ShivamWho <shivam2094.be20@chitkara.edu.in>
2021-07-06 00:17:04 +0530

    some edits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,20 @@ import Note from "./Note";
 import Footer from "./Footer";
 import notes from "../notes";
 
-
-
-function App(){
-    return (
-        <div>
-            <Header/>
-            {notes.map(
-                noteTerm =>(
-                    <Note
-                        key={noteTerm.key}
-                        title={noteTerm.title}
-                        content={noteTerm.content}
-                    />
-                ))}
-            <Footer />
-        </div>
-    );
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((noteTerm) => (
+        <Note
+          key={noteTerm.key}
+          title={noteTerm.title}
+          content={noteTerm.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-function Footer(){
-    let currentTime = new Date();
-    let year = currentTime.getFullYear();
+function Footer() {
+  let currentTime = new Date();
+  let year = currentTime.getFullYear();
 
-    return(
-        <footer>
-            <p>Copyright ⓒ {year}</p>
-        </footer>
-    );
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;

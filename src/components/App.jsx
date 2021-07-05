018affce271f51aa512e1e3f80adb116683afcfa
ShivamWho <shivam2094.be20@chitkara.edu.in>
2021-07-05 23:59:29 +0530

    add more notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
+
+
 
 function App(){
     return (
         <div>
             <Header/>
-            <Note />
+            {notes.map(
+                noteTerm =>(
+                    <Note
+                        key={noteTerm.key}
+                        title={noteTerm.title}
+                        content={noteTerm.content}
+                    />
+                ))}
             <Footer />
         </div>
     );
